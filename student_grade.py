0ad7126db207bf0151771a2cd823cd497c07a56d
balamurali899 <47619218+balamurali899@users.noreply.github.com>
2019-05-06 11:00:15 +0530

    Create student_grade.py

---
+++ student_grade.py
@@ -0,0 +1,11 @@
+stu_mark=77
+if(stu_mark>=80 and stu_mark<=100):
+    print("A grade")
+elif(stu_mark>=73 and stu_mark<=79):
+    print("B grade")
+elif(stu_mark>=65 and stu_mark<=72):
+    print("C grade")
+elif(stu_mark>=0 and stu_mark<=64):
+    print("D grade")
+else:
+    print("Z grade")
